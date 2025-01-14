f08f04ad9f9cfe16dbde3b628177aa24b25bb21d
Pedro Chinen <chinen93@gmail.com>
2025-01-14 16:39:53 -0500

    Add game logic

--- src/Board.js
+++ src/Board.js
@@ -1,17 +1,58 @@
 import { useState } from 'react';
 import Square from "./Square";
 
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2], // top row
+        [3, 4, 5], // middle row
+        [6, 7, 8], // botton row
+        [0, 3, 6], // left column
+        [1, 4, 7], // middle column
+        [2, 5, 8], // right column
+        [0, 4, 8], // major diagonal
+        [2, 4, 6]  // minor diagonal
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+        }
+    }
+    return null;
+}
+
 export default function Board() {
+    const [xIsNext, setXIsNext] = useState(true);
     const [squares, setSquares] = useState(Array(9).fill(null));
 
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+        status = "Winner: " + winner;
+    } else {
+        status = "Next player: " + (xIsNext ? "X" : "O");
+    }
+
     function handleClick(i) {
+        if (squares[i] || calculateWinner(squares)) {
+            return;
+        }
+
         const nextSquares = squares.slice();
-        nextSquares[i] = "X";
+
+        if (xIsNext) {
+            nextSquares[i] = "X";
+        } else {
+            nextSquares[i] = "O";
+        }
+
         setSquares(nextSquares);
+        setXIsNext(!xIsNext);
     }
 
     return (
     <>
+        <div className="status">{status}</div>
         <div className="board-row">
             <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
             <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
